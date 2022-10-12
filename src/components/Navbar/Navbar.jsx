c163d588fb8036712c5a85a66993bbeeaeb797f8
Yevhen <54176294+yevki@users.noreply.github.com>
2022-10-12 13:03:21 +0300

    Add files via upload
    Added routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
-
-let c1 = "item";
-let c2 = "active";
-// "item active"
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${c2}`;
-
 
 const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}>
-            <a>Profile</a>
+        <div>
+            <NavLink to='/profile' className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
         </div>
-        <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+        <div>
+            <NavLink to='/dialogs' className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
         </div>
-        <div className={s.item}>
-            <a>News</a>
+        <div>
+            <NavLink to='/news' className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
         </div>
-        <div className={s.item}>
-            <a>Music</a>
+        <div>
+            <NavLink to='/music' className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
         </div>
-        <div className={s.item}>
-            <a>Settings</a>
+        <div>
+            <NavLink to='/settings' className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
         </div>
     </nav>
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
     <div>
       <img src='https://www.dumka.poltava.ua/wp-content/uploads/2022/02/1000.jpg' />
     </div>
